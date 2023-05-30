fb9bef40fa3acf0069f419e4dd5d8de49fd9af3d
Darkchinata <130670586+Darkchinata@users.noreply.github.com>
2023-05-30 09:26:56 -0400

    Update Challenge Alura.js

--- Challenge Alura.js
+++ Challenge Alura.js
@@ -20,73 +20,72 @@ function encriptarTexto(e) {
     let palabras = texto.split(' ');
     let nuevasPalabras = [];
     
-  for (let palabra of palabras) {
-    palabra = palabra.replaceAll('e','enter');
-    palabra = palabra.replaceAll('i','imes');
-    palabra = palabra.replaceAll('a','ai');
-    palabra = palabra.replaceAll('o','ober');
-    palabra = palabra.replaceAll('u','ufat');  
-    nuevasPalabras.push(palabra);
-  }
+    for (let palabra of palabras) {
+      palabra = palabra.replaceAll('e','enter');
+      palabra = palabra.replaceAll('i','imes');
+      palabra = palabra.replaceAll('a','ai');
+      palabra = palabra.replaceAll('o','ober');
+      palabra = palabra.replaceAll('u','ufat');      
+      
+      nuevasPalabras.push(palabra);    
+    }
 
-  const resultado = nuevasPalabras.join(' '); 
-  inputResultado.value = resultado;
-  }
-  
-  else {
-    mostrarError('ATENCION!!! Sólo se permiten letras minúsculas, sin tildes');
+    const resultado = nuevasPalabras.join(' ');
+    
+    inputResultado.value = resultado;
+  } else {
+    mostrarError('Recuerda! Sólo se permiten letras minúsculas, sin tildes');
     return;
-  }
+  }  
 }
 
 function desencriptarTexto(e) {
-  e.preventDefault();
+  e.preventDefault();  
   inputResultado.value = '';
   let texto = inputTexto.value;
 
   if(texto.match(soloLetras)!=null){
     let palabras = texto.split(" ");
-    let nuevasPalabras = [];
+    let nuevasPalabras = [];    
 
-  for (let palabra of palabras) {
-    palabra = palabra.replaceAll('ai','a');
-    palabra = palabra.replaceAll('enter','e');
-    palabra = palabra.replaceAll('imes','i');
-    palabra = palabra.replaceAll('ober','o');
-    palabra = palabra.replaceAll('ufat','u');
-    nuevasPalabras.push(palabra);
-  }
+    for (let palabra of palabras) {
+      palabra = palabra.replaceAll('enter','e');
+      palabra = palabra.replaceAll('imes','i');
+      palabra = palabra.replaceAll('ai','a');
+      palabra = palabra.replaceAll('ober','o');
+      palabra = palabra.replaceAll('ufat','u');
+      nuevasPalabras.push(palabra);    
+    }
 
-  const resultado = nuevasPalabras.join(' ');
-
-  inputResultado.value = resultado;
-  }
-  
-  else {
-    mostrarError('ATENCION!!! Sólo se permiten letras minúsculas, sin tildes');
+    const resultado = nuevasPalabras.join(' ');
+    
+    inputResultado.value = resultado;
+  } else {
+    mostrarError('Recuerda! Sólo se permiten letras minúsculas, sin tildes');
     return;
-  }
+  }  
 }
 
 function mostrarError(mensaje) {
   const existeError = document.querySelector('.error');
-
+  
   if(!existeError) {
-    const formulario = document.getElementById('formulario');
-    const divMensaje = document.createElement('div');
-    divMensaje.classList.add('error');
+      const formulario = document.getElementById('formulario');
+      const divMensaje = document.createElement('div');
+      divMensaje.classList.add('error');
   
-    divMensaje.textContent = mensaje;
-    formulario.appendChild(divMensaje);
-  
-    setTimeout(()=> {
-      divMensaje.remove();
-    }, 3000);
+      divMensaje.textContent = mensaje;            
+      formulario.appendChild(divMensaje);
+      
+      setTimeout(()=> {
+          divMensaje.remove();
+      }, 3000);
   }
 }
 
 function copiarTexto(e) {
-  e.preventDefault(); 
-  const mensaje = inputResultado.value;
-  navigator.clipboard.writeText(mensaje);
+    e.preventDefault(); 
+    const mensaje = inputResultado.value;
+
+    navigator.clipboard.writeText(mensaje);
 }
